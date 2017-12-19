29ecb83d431b7f71735ff547837692376efc581c
deane163 <deane163@126.com>
2017-12-19 20:01:11 +0800

    【功能】添加验证客户端合法性的注解

---
+++ xiaoshu-authorization/src/main/java/com/xiaoshu/annotation/ClientAuthorization.java
@@ -0,0 +1,48 @@
+package com.xiaoshu.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * 
+ * code is far away from bug with the animal protecting
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ *
+ * @Description : 
+ * ---------------------------------
+ * @Author : deane.administrator
+ * @Date : Create in 2017年12月19日下午8:00:29
+ * 
+ * Copyright (C)2013-2017 小树盛凯科技 All rights reserved.
+ */
+@Inherited
+@Target({TYPE, METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ClientAuthorization {
+
+}
