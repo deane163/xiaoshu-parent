08af80a66b725532aa39a19be784272c621e63e4
deane163 <deane163@126.com>
2017-12-25 17:29:39 +0800

    【功能】添加注解信息

--- xiaoshu-core/src/main/java/com/xiaoshu/rest/RestTemplateConfig.java
+++ xiaoshu-core/src/main/java/com/xiaoshu/rest/RestTemplateConfig.java
@@ -27,7 +27,7 @@ import org.springframework.web.client.RestTemplate;
  * 　　　┗┻┛　┗┻┛
  *
  *
- * @Description : 
+ * @Description : RestTemplate 服务类
  * ---------------------------------
  * @Author : deane.administrator
  * @Date : Create in 2017年12月13日下午3:39:19
